b7b631988f6ac8e8aa984af0e2032bafec45120a
Adrian Pomareta <adrianpomaretagit@gmail.com>
2025-09-24 14:09:57 +0200

    ejercicio_5_2

--- primerKotlin/src/Ejercicios/Ejercicio5.kt
+++ primerKotlin/src/Ejercicios/Ejercicio5.kt
@@ -13,21 +13,37 @@ RESISTENCIA---> R=V/I
 
 fun main() {
     println("Calculadora Ley de Ohm")
+    println("escribe la tension del circuito")
 
-    println("Que queres calcular :  \n escribe: tension, corriente, o resistencia ")
-    val calculo : String? = readLine().toString()
+    val v : Double? = readLine()?.toDoubleOrNull()
 
+    println("escribe la resistencia del circuito")
+    val r : Double? = readLine()?.toDoubleOrNull()
 
-    if(calculo == "tension") {
-        println("tension")
-    } else if (calculo == "corriente") {
-        println("corriente")
-    }else if (calculo == "resistencia") {
-        println("resistencia")
-    } else {
-        println("Escribe: tension, corriente, o resistencia ")
+    println("escribe la corriente del circuito")
+    val i : Double? = readLine()?.toDoubleOrNull()
+
+    println("El resultado es: ${calculoLeyOhm(v, r, i)}")
+}
+
+fun calculoLeyOhm (V: Double?, R: Double?, I: Double?) : Any? {
+
+    if( V == null && R != null && I != null){
+
+        return println("La tension es: ${redondear(I*R)}, Voltios")
+    } else if (R == null && V != null && I != null) {
+
+        return println("La resistencia es: ${redondear(V/I)}, Ohmios")
+    } else if (I == null && V != null && R != null) {
+
+        return println("La corriente es: ${redondear(V/R)}, Amperios")
+    }else {
+        return "Valores inválidos, Escribe al menos dos parametros"
     }
+}
 
-
-
+// Función para redondear a 2 decimales y devolver como String
+fun redondear(valor: Double): Double? {
+    val resultado = kotlin.math.round(valor * 100) / 100
+    return resultado
 }
